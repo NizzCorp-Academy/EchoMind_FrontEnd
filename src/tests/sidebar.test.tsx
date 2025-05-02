4f9d3637d8eb57120ef015f54681aad3096c0356
Jaseem <muhammadjaseem791@gmail.com>
2025-05-02 07:52:24 +0530

    fix ( side bar ) fix side bar test case error
    fixed the error on the side bar test file made sure it ran as expected
    
    - modified tests/sidebar.test.tsx
    - changed the mocking way to make sure it worked as expected

--- src/tests/sidebar.test.tsx
+++ src/tests/sidebar.test.tsx
@@ -4,41 +4,37 @@ import { MemoryRouter, Route, Routes } from "react-router";
 import Sidebar from "../components/Sidebar";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
-import { useDeleteChat } from "@/hooks/chatHook";
+import { useGetChats, useDeleteChat, useEditChat } from "../hooks/chatHook";
 
 const ChatScreenMock = () => <h1>Hello User</h1>;
-// vi.mock("../hooks/chatHook", async () => {
-//     const originalModule = await vi.importActual("../hooks/chatHook");
-//     return {
-//         ...originalModule,
-//         default: {
-//             useGetChats: vi.fn().mockReturnValue({
-//                 chats: [],
-//                 isGettingChat: false,
-//                 getChats: vi.fn(),
-//             }),
-//             useDeleteChat: vi.fn().mockReturnValue({
-//                 deleteChat: vi.fn(),
-//                 isDelettingMessage: false,
-//             }),
-//             useEditChat: vi.fn().mockReturnValue({
-//                 editChat: vi.fn(),
-//                 isUpdattingChat: false,
-//             }),
-//         },
-//     };
-// });
 
-vi.mock("../hooks/userHook", () => ({
+vi.mock("../hooks/chatHook", () => ({
     useDeleteChat: vi.fn(),
-    
+    useEditChat: vi.fn(),
+    useGetChats: vi.fn(),
 }));
 
 describe("Sidebar Component", () => {
     beforeEach(() => {
         vi.clearAllMocks();
-    });
 
+        vi.mocked(useGetChats).mockReturnValue({
+            chats: [],
+            isGettingChat: false,
+            getChats: vi.fn(),
+        });
+
+        vi.mocked(useDeleteChat).mockReturnValue({
+            chats: [],
+            deleteChat: vi.fn(),
+            isDelettingMessage: false,
+        });
+        vi.mocked(useEditChat).mockReturnValue({
+            chats: [],
+            editChat: vi.fn(),
+            isUpdattingChat: false,
+        });
+    });
     const setup = () =>
         render(
             <Provider store={store}>
@@ -60,116 +56,117 @@ describe("Sidebar Component", () => {
         expect(screen.getByText("No chat Found")).toBeInTheDocument();
     });
 
-    // it("shows 'Updating Chat Title...' when isUpdattingChat is true", async () => {
-    //     const chatHook = await import("../hooks/chatHook");
-    //     (chatHook.default as any).mockImplementation(() => ({
-    //         useGetChats: vi.fn().mockReturnValue({
-    //             chats: [],
-    //             isGettingChat: false,
-    //             getChats: vi.fn(),
-    //         }),
-    //         useDeleteChat: vi.fn().mockReturnValue({
-    //             deleteChat: vi.fn(),
-    //             isDelettingMessage: false,
-    //         }),
-    //         useEditChat: vi.fn().mockReturnValue({
-    //             editChat: vi.fn(),
-    //             isUpdattingChat: true,
-    //         }),
-    //     }));
+    it("shows 'Updating Chat Title...' when isUpdattingChat is true", async () => {
+        vi.mocked(useGetChats).mockReturnValue({
+            chats: [],
+            isGettingChat: false,
+            getChats: vi.fn(),
+        });
+        vi.mocked(useDeleteChat).mockReturnValue({
+            chats: [],
+            deleteChat: vi.fn(),
+            isDelettingMessage: false,
+        });
+        vi.mocked(useEditChat).mockReturnValue({
+            chats: [],
+            editChat: vi.fn(),
+            isUpdattingChat: true,
+        });
 
-    //     setup();
-    //     expect(await screen.findByText("Updating Chat Title...")).toBeInTheDocument();
-    // });
+        setup();
+        expect(
+            await screen.findByText("Updating Chat Title...")
+        ).toBeInTheDocument();
+    });
 
-    // it("should call useEditChat and update the chat title", async () => {
-    //     const editChatMock = vi.fn();
-    //     const getChatsMock = vi.fn();
-    //     const chatHook = await import("../hooks/chatHook");
-    //     (chatHook.default as any).mockImplementation(() => ({
-    //         useGetChats: vi.fn().mockReturnValue({
-    //             chats: [
-    //                 {
-    //                     _id: "chatId",
-    //                     title: "Old Title",
-    //                 },
-    //             ],
-    //             isGettingChat: false,
-    //             getChats: getChatsMock,
-    //         }),
-    //         useDeleteChat: vi.fn().mockReturnValue({
-    //             deleteChat: vi.fn(),
-    //             isDelettingMessage: false,
-    //         }),
-    //         useEditChat: vi.fn().mockReturnValue({
-    //             editChat: editChatMock,
-    //             isUpdattingChat: false,
-    //         }),
-    //     }));
-    //     setup();
-    //     const threeDot = screen.getByTestId("threeDot");
-    //     fireEvent.click(threeDot);
+    it("should call useEditChat and update the chat title", async () => {
+        const editChatMock = vi.fn();
+        const getChatsMock = vi.fn();
+        vi.mocked(useGetChats).mockReturnValue({
+            chats: [
+                {
+                    _id: "chatId",
+                    title: "Old Title",
+                    createdAt: "lskdjflsd",
+                    updatedAt: "lsdjflsd",
+                },
+            ],
+            isGettingChat: false,
+            getChats: getChatsMock,
+        });
+        vi.mocked(useEditChat).mockReturnValue({
+            chats: [],
+            editChat: editChatMock,
+            isUpdattingChat: false,
+        });
 
-    //     const renameOption = await screen.findByText("Rename");
-    //     fireEvent.click(renameOption);
+        setup();
+        const threeDot = screen.getByTestId("threeDot");
+        fireEvent.click(threeDot);
 
-    //     const input = screen.getByDisplayValue("Old Title");
-    //     fireEvent.change(input, { target: { value: "New Title" } });
+        const renameOption = await screen.findByText("Rename");
+        fireEvent.click(renameOption);
 
-    //     const saveButton = screen.getByText("Save");
-    //     fireEvent.click(saveButton);
+        const input = screen.getByDisplayValue("Old Title");
+        fireEvent.change(input, { target: { value: "New Title" } });
 
-    //     expect(editChatMock).toHaveBeenCalledWith("chatId", "New Title");
-    // });
+        const saveButton = screen.getByText("Save");
+        fireEvent.click(saveButton);
 
-    // it("should call useDeleteChat and delete the chat", async () => {
-    //     const deleteChatMock = vi.fn();
-    //     const getChatsMock = vi.fn();
-    //     const chatHook = await import("../hooks/chatHook");
-    //     (chatHook.default as any).mockImplementation(() => ({
-    //         useGetChats: vi.fn().mockReturnValue({
-    //             chats: [
-    //                 {
-    //                     _id: "chatId",
-    //                     title: "Old Title",
-    //                 },
-    //             ],
-    //             isGettingChat: false,
-    //             getChats: getChatsMock,
-    //         }),
-    //         useDeleteChat: vi.fn().mockReturnValue({
-    //             deleteChat: deleteChatMock,
-    //             isDelettingMessage: false,
-    //         }),
-    //         useEditChat: vi.fn().mockReturnValue({
-    //             editChat: vi.fn(),
-    //             isUpdattingChat: false,
-    //         }),
-    //     }));
-    //     setup();
-    //     const threeDot = screen.getByTestId("threeDot");
-    //     fireEvent.click(threeDot);
+        expect(editChatMock).toHaveBeenCalledWith("chatId", "New Title");
+    });
 
-    //     const deleteButton = await screen.findByText("Delete");
-    //     fireEvent.click(deleteButton);
+    it("should call useDeleteChat and delete the chat", async () => {
+        const deleteChatMock = vi.fn();
+        vi.mocked(useGetChats).mockReturnValue({
+            chats: [
+                {
+                    _id: "chatId",
+                    title: "Old Title",
+                    createdAt: "lskdjflsd",
+                    updatedAt: "lsdjflsd",
+                },
+            ],
+            isGettingChat: false,
+            getChats: vi.fn(),
+        });
+        vi.mocked(useDeleteChat).mockReturnValue({
+            chats: [],
+            deleteChat: deleteChatMock,
+            isDelettingMessage: false,
+        });
+        setup();
+        const threeDot = screen.getByTestId("threeDot");
+        fireEvent.click(threeDot);
 
-    //     expect(deleteChatMock).toHaveBeenCalledWith("chatId");
-    // });
+        const deleteButton = await screen.findByText("Delete");
+        fireEvent.click(deleteButton);
 
-    // it("calls navigate when clicking the New Chat button", async () => {
-    //     render(
-    //         <Provider store={store}>
-    //             <MemoryRouter initialEntries={["/chats/123"]}>
-    //                 <Routes>
-    //                     <Route path="/chats/123" element={<Sidebar isOpen={true} toggleSideBar={() => {}} />} />
-    //                     <Route path="/" element={<ChatScreenMock />} />
-    //                 </Routes>
-    //             </MemoryRouter>
-    //         </Provider>
-    //     );
-    //     const button = screen.getByText("New Chat");
-    //     fireEvent.click(button);
+        expect(deleteChatMock).toHaveBeenCalledWith("chatId");
+    });
 
-    //     expect(await screen.findByText(/Hello User/i)).toBeInTheDocument();
-    // });
+    it("calls navigate when clicking the New Chat button", async () => {
+        render(
+            <Provider store={store}>
+                <MemoryRouter initialEntries={["/chats/123"]}>
+                    <Routes>
+                        <Route
+                            path="/chats/123"
+                            element={
+                                <Sidebar
+                                    isOpen={true}
+                                    toggleSideBar={() => {}}
+                                />
+                            }
+                        />
+                        <Route path="/" element={<ChatScreenMock />} />
+                    </Routes>
+                </MemoryRouter>
+            </Provider>
+        );
+        const button = screen.getByText("New Chat");
+        fireEvent.click(button);
+
+        expect(await screen.findByText(/Hello User/i)).toBeInTheDocument();
+    });
 });
