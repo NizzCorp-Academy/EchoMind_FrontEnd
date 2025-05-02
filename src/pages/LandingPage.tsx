a51b56cec29a9e6ed86adcd91ccc3f544bc5fd46
HASINMMPM <hasinmmpm786@gmail.com>
2025-05-02 16:51:30 +0530

    feat(landingPage): implemented responsive design for improved user experience
    Applied responsive design to the Landing Page to ensure optimal layout and usability across various device sizes. Enhances accessibility and user experience on both desktop and mobile platforms.
    #Closed :INK-115 https://nizzcorp-i.atlassian.net/browse/OBI-115?atlOrigin=eyJpIjoiZWRhZTVlMDc1OTBiNDA0MWIzMDlhMWZjN2ZiODA4OGYiLCJwIjoiaiJ9

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,6 +7,8 @@ import user_speak from "../assets/svg/user-speaking 1.svg";
 import world from "../assets/svg/world.svg";
 import robo from "../assets/robo2.jpg";
 import { motion } from "framer-motion";
+import logo from "../assets/logo.png";
+import bottomLogo from "../assets/NizzCorp.png";
 
 export const LandingPage = () => {
   const navigate = useNavigate();
@@ -51,17 +53,17 @@ export const LandingPage = () => {
         </div>
         <div
           id="home"
-           data-testid="home"
-          className=" flex items-center justify-between px-[100px]"
+          data-testid="home"
+          className="flex flex-col md:flex-row items-center justify-between pt-16 px-6 lg:px-[100px]"
         >
           <motion.div
             initial={{ x: -200, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 0.6, delay: 0 }}
-            className="h-[320px] flex flex-col justify-between "
+            className=" flex flex-col justify-between gap-10 lg:gap-16"
           >
             <div className="overflow-y-clip max-w-[500px] space-y-[15px]">
-              <div className="text-5xl font-bold">
+              <div className="section-title">
                 Get ready for the new era of AI
               </div>
               <div className="text-sm">
@@ -72,7 +74,8 @@ export const LandingPage = () => {
             </div>
             <div className="space-y-[18px]">
               <div className="bg-gradient-to-r from-[#8A4CEF] to-[#2A0070] h-[50px] flex items-center w-[150px] justify-center rounded-[16px] p-[1.5px]">
-                <div data-testid="get-start"
+                <div
+                  data-testid="get-start"
                   className="bg-gradient-to-r from-[#6E27E0] to-[#460F9E] h-full flex items-center w-full justify-center rounded-[16px] cursor-pointer"
                   onClick={() => navigate("/register")}
                 >
@@ -97,20 +100,19 @@ export const LandingPage = () => {
       {/* FEATURE START */}
       <section
         id="features"
-         data-testid="features"
+        data-testid="features"
         className="w-full h-full flex flex-col items-start px-12 py-9 bg-radial-top-left  bg-[#0B0C0F] gap-2 "
       >
         <h3 className="font-semibold">Features</h3>
-        <h2 className="text-white font-bold  text-4xl">
-          Say Hello to Smarter Support
-        </h2>
+        <h2 className="section-title">Say Hello to Smarter Support</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-6">
           <div className="flex flex-col gap-4 my-5 md:my-0">
             {lists.map((list, index) => (
-              <motion.div
-                initial={{ x: -200, opacity: 0 }}
-                whileInView={{ x: 0, opacity: 1 }}
-                transition={{ duration: 0.6, delay: 0.3 }}
+             <motion.div
+             initial={{ x: -200, opacity: 0 }}
+             whileInView={{ x: 0, opacity: 1 }}
+             viewport={{ once: true }} 
+             transition={{ duration: 0.6, delay: 0.3 }}
                 key={index}
                 className="flex flex-row items-start gap-4 p-4 rounded-xl shadow-md"
               >
@@ -129,14 +131,12 @@ export const LandingPage = () => {
       </section>
       {/* FEATURE eND */}
       {/* Footer Section */}
-      <div className="w-full h-full flex flex-col items-start px-12 py-8 bg-radial-top-left  bg-[#0B0C0F] gap-2 ">
-        <div className="font-semibold" id="about"  data-testid="about">
+      <div className="w-full h-full flex flex-col items-start px-12 py-9 bg-radial-top-left  bg-[#0B0C0F] gap-2 ">
+        <div className="font-semibold" id="about" data-testid="about">
           ABOUT US
         </div>
-        <h1 className="text-4xl font-semibold">
-          Powered by Nizzcorp, Built for You
-        </h1>
-        <div className="font-medium w-[75%]">
+        <h1 className="section-title">Powered by Nizzcorp, Built for You</h1>
+        <div className="font-medium w-full  md:w-[75%]">
           At NizzCorp, we believe in building technology that feels like magic —
           fast, smart, and human. Founded with a passion for innovation and a
           deep curiosity for the future, NizzCorp is a modern tech company
@@ -146,51 +146,59 @@ export const LandingPage = () => {
           latest project harnesses the capabilities of cutting large language
         </div>
       </div>
-      <div  data-testid="footer" className="w-full h-full flex flex-col items-start pt-4 px-12 pb-8 bg-radial-top-left  bg-[#0B0C0F]">
-        <img src="./logo.png" className="w-44" />
-        <div className="flex gap-[107px] items-center py-8 px-2">
-          <div className="space-y-3.5">
-            <div className="font-semibold">Company</div>
-            <ul className="text-sm space-y-1.5">
-              <li>About</li>
-              <li>Services</li>
-              <li>Features</li>
-              <li>Pricing</li>
-            </ul>
+      <div
+        data-testid="footer"
+        className="w-full h-full flex flex-col items-start pt-4 px-12 pb-8 bg-radial-top-left  bg-[#0B0C0F]"
+      >
+        <img src={logo} className="w-44" />
+        {/* bottom */}
+        <div className="flex flex-col-reverse lg:flex-row w-full lg:gap-20">
+          {/* Links */}
+          <div className="flex flex-col md:flex-row gap-12 md:gap-20 items-start md:items-center py-8 px-2">
+            <div className="space-y-3.5">
+              <div className="font-semibold">Company</div>
+              <ul className="text-sm space-y-1.5">
+                <li>About</li>
+                <li>Services</li>
+                <li>Features</li>
+                <li>Pricing</li>
+              </ul>
+            </div>
+            <div className="space-y-3.5">
+              <div className="font-semibold">Resources </div>
+              <ul className="text-sm space-y-1.5">
+                <li>Case Studies</li>
+                <li>Webinars</li>
+                <li>Developer</li>
+                <li>Documentation</li>
+              </ul>
+            </div>
+            <div className="space-y-0 md:space-y-3.5">
+              <div className="font-semibold">Help </div>
+              <ul className="text-sm space-y-1.5">
+                <li>Customer Support</li>
+                <li>Delivery Details</li>
+                <li>Terms & Conditions</li>
+                <li>Privacy Policy</li>
+              </ul>
+            </div>
           </div>
-          <div className="space-y-3.5">
-            <div className="font-semibold">Resources </div>
-            <ul className="text-sm space-y-1.5">
-              <li>Case Studies</li>
-              <li>Webinars</li>
-              <li>Developer</li>
-              <li>Documentation</li>
-            </ul>
-          </div>
-          <div className="space-y-3.5">
-            <div className="font-semibold">Help </div>
-            <ul className="text-sm space-y-1.5">
-              <li>Customer Support</li>
-              <li>Delivery Details</li>
-              <li>Terms & Conditions</li>
-              <li>Privacy Policy</li>
-            </ul>
-          </div>
-          <div className="h-full space-y-5 pl-[37px] flex flex-col ">
+          {/* Email */}
+          <div className="h-full space-y-5 flex flex-col py-8 ">
             <div className="font-semibold">Subscribe To Newsletter</div>
-            <div className=" flex gap-3 items-start justify-center  ">
+            <div className=" flex flex-col sm:flex-row gap-3 items-start justify-center  text-sm ">
               <input
-                className="bg-gradient-to-r from-[#2B2929] to-[#3A3A3A] w-76 outline-none border-2 border-[#1F1C1C] py-4 px-5.5 rounded-full"
+                className="bg-gradient-to-r from-[#2B2929] to-[#3A3A3A]  outline-none border-2 border-[#1F1C1C] p-3  rounded-full w-full lg:min-w-3xs"
                 placeholder="Enter Email Address"
               />
-              <button className="bg-[#571ABC] font-semibold rounded-full py-4 px-6">
+              <button className="bg-[#571ABC] font-semibold rounded-full py-4 px-6 w-full sm:w-1/4 lg:w-full">
                 SUBSCRIBE
               </button>
             </div>
           </div>
         </div>
-        <div className="px-20 py-10">
-          <img src="./NizzCorp.png" className="" />
+        <div className="px-3 md:px-20 py-10">
+          <img src={bottomLogo} className="" />
         </div>
       </div>
     </div>
