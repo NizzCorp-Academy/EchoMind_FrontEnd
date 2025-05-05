223a824a7bad5e3296aa7e31a8024bf910c946cb
Jaseem <muhammadjaseem791@gmail.com>
2025-05-05 10:21:21 +0530

    refact ( chat service )  separate types from chatService
    separated types from chat service into the chat types file
    
    - modified services/chatService.ts
    - modified types/chat.types.ts

--- src/services/chatService.ts
+++ src/services/chatService.ts
@@ -13,6 +13,11 @@
  */
 
 import {
+    AxiosChatResponse,
+    AxiosErrorRespnse,
+    ChatFormData,
+} from "@/types/chat.types";
+import {
     chat_Completion,
     delete_Chat,
     delete_Message,
@@ -23,78 +28,6 @@ import {
 import AxiosClass from "../utils/axios";
 
 AxiosClass.initializeInterceptors();
-
-/**
- * @interface AxiosResponse
- * @brief Represents a generic API response.
- */
-interface AxiosResponse {
-    status: string;
-}
-
-/**
- * @interface AxiosErrorRespnse
- * @brief Represents an error response from the API.
- */
-interface AxiosErrorRespnse extends AxiosResponse {
-    errorCode: string;
-    message: string;
-}
-
-/**
- * @interface AxiosUserResponse
- * @brief Represents a user-related API response.
- */
-export interface AxiosUserResponse extends AxiosResponse {
-    user: {
-        username: string;
-        email: string;
-    };
-    token: string;
-}
-
-/**
- * @interface AxiosChatResponse
- * @brief Represents a chat-related API response.
- */
-interface AxiosChatResponse extends AxiosResponse {
-    response?: {
-        userMessage: {
-            _id: string;
-            isFromUser: boolean;
-            message: string;
-            chatId: string;
-        };
-
-        responseMessage: {
-            _id: string;
-            isFromUser: boolean;
-            message: string;
-            chatId: string;
-        };
-    };
-    chats?: {
-        title: string;
-        _id: string;
-        createdAt: string;
-        updatedAt: string;
-    }[];
-    messages?: {
-        _id: string;
-        isFromUser: boolean;
-        message: string;
-        chatId: string;
-    }[];
-}
-
-/**
- * @interface ChatFormData
- * @brief Represents the form data for chat-related operations.
- */
-interface ChatFormData {
-    title?: string;
-    prompt?: string;
-}
 
 /**
  * @class ChatService
--- src/types/chat.types.ts
+++ src/types/chat.types.ts
@@ -21,3 +21,75 @@ export interface ChatState {
           }[]
         | undefined;
 }
+
+/**
+ * @interface AxiosResponse
+ * @brief Represents a generic API response.
+ */
+export interface AxiosResponse {
+    status: string;
+}
+
+/**
+ * @interface AxiosErrorRespnse
+ * @brief Represents an error response from the API.
+ */
+export interface AxiosErrorRespnse extends AxiosResponse {
+    errorCode: string;
+    message: string;
+}
+
+/**
+ * @interface AxiosUserResponse
+ * @brief Represents a user-related API response.
+ */
+export interface AxiosUserResponse extends AxiosResponse {
+    user: {
+        username: string;
+        email: string;
+    };
+    token: string;
+}
+
+/**
+ * @interface AxiosChatResponse
+ * @brief Represents a chat-related API response.
+ */
+export interface AxiosChatResponse extends AxiosResponse {
+    response?: {
+        userMessage: {
+            _id: string;
+            isFromUser: boolean;
+            message: string;
+            chatId: string;
+        };
+
+        responseMessage: {
+            _id: string;
+            isFromUser: boolean;
+            message: string;
+            chatId: string;
+        };
+    };
+    chats?: {
+        title: string;
+        _id: string;
+        createdAt: string;
+        updatedAt: string;
+    }[];
+    messages?: {
+        _id: string;
+        isFromUser: boolean;
+        message: string;
+        chatId: string;
+    }[];
+}
+
+/**
+ * @interface ChatFormData
+ * @brief Represents the form data for chat-related operations.
+ */
+export interface ChatFormData {
+    title?: string;
+    prompt?: string;
+}
